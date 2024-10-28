d70fc0d42dfcbc58d661f55e25736112d77eeea0
Katielly Reis <77698539+Katielly@users.noreply.github.com>
2024-10-27 21:09:06 -0400

    chore: add troca de tela ao click

--- app/src/main/java/com/example/androidflix/MainActivityLogin.kt
+++ app/src/main/java/com/example/androidflix/MainActivityLogin.kt
@@ -1,20 +1,24 @@
 package com.example.androidflix
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivityLogin : AppCompatActivity() {
+    private lateinit var btnEntrar: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        btnEntrar = findViewById(R.id.button4)
+
+        btnEntrar.setOnClickListener {
+            val nextPage = Intent(this, MainActivityPerfil::class.java)
+            startActivity(nextPage)
         }
     }
 }
